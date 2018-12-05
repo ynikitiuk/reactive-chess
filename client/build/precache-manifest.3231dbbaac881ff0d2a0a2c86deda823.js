1ec5d57ba8003856d7812ff1464a528458ed6070
Yurii Nikitiuk <y.nikitiuk@gmail.com>
2018-12-05 19:15:52 +0200

    added `build` folder

---
+++ client/build/precache-manifest.3231dbbaac881ff0d2a0a2c86deda823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4cb95d64c617bcf10574",
+    "url": "/static/js/main.4cb95d64.chunk.js"
+  },
+  {
+    "revision": "3ff635532085f3e3bdbd",
+    "url": "/static/js/1.3ff63553.chunk.js"
+  },
+  {
+    "revision": "4cb95d64c617bcf10574",
+    "url": "/static/css/main.3ba48514.chunk.css"
+  },
+  {
+    "revision": "5893ccb59b8ed274175825f31c20071f",
+    "url": "/index.html"
+  }
+];
